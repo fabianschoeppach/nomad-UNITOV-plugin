2ae5b24793675664ab3cc378ff30dd6b4b0ed52e
Simon Ternes <ternes@ing.uniroma2.it>
2024-10-29 14:54:19 +0100

    MPP parser

--- src/nomad_unitov_plugin/schema_packages/file_parser/mppt_simple.py
+++ src/nomad_unitov_plugin/schema_packages/file_parser/mppt_simple.py
@@ -22,35 +22,42 @@ def read_mppt_file(filedata):
 
     df = pd.read_csv(
         StringIO(filedata),
-        skiprows=0,
-        nrows=5,
+        skiprows=0,s
+        nrows=41,
         header=None,
-        sep=':\t',
+        sep='\t',
         index_col=0,
         engine='python',
-        encoding='unicode_escape')
+        encoding='unicode_escape').T
+    
     df_curve = pd.read_csv(
         StringIO(filedata),
-        header=6,
-        skiprows=[7],
+        skiprows=42,
         sep='\t',
         encoding='unicode_escape',
         engine='python')
     df_curve = df_curve.dropna(how='any', axis=0)
 
-    mppt_dict = {}
-    mppt_dict['total_time'] = get_value(df.iloc[0, 0])
-    mppt_dict['step_size'] = get_value(df.iloc[1, 0])
-    mppt_dict['time_per_track'] = get_value(df.iloc[2, 0])
-    mppt_dict['active_area'] = get_value(df.iloc[3, 0])
-    mppt_dict['voltage'] = get_value(df.iloc[4, 0])
+    print(df.T,df_curve)
 
-    mppt_dict['time_data'] = np.array(df_curve["time"], dtype=np.float64)
-    mppt_dict['voltage_data'] = np.array(df_curve["voltage"], dtype=np.float64)
-    mppt_dict['current_density_data'] = np.array(df_curve["current density"], dtype=np.float64)
-    mppt_dict['power_data'] = np.array(df_curve["power"], dtype=np.float64)
+    mppt_dict = {}
+    mppt_dict['total_time'] = get_value(df['Test duration (hours)'].iloc[0]*60*60)
+    mppt_dict['step_size'] = get_value(df['JV interval (min)'].iloc[0]*60)
+    mppt_dict['time_per_track'] = get_value(df['track delay (s)'].iloc[0])
+    mppt_dict['active_area'] = get_value(df['Cell Area (cm2)'].iloc[0])
+    #mppt_dict['voltage'] = get_value(df.iloc[4, 0])
+
+    mppt_dict['time_data'] = np.array(df_curve["Time (hours)"], dtype=np.float64)
+    mppt_dict['voltage_data'] = np.array(df_curve["V (V)"], dtype=np.float64)
+    mppt_dict['current_density_data'] = np.array(df_curve["J (mAcm-2)"], dtype=np.float64)
+    mppt_dict['power_data'] = np.array(df_curve["P (mWcm-2)"], dtype=np.float64)
 
     return mppt_dict
+
+import os,io;
+abs_path = os.path.dirname(os.path.abspath(__file__))+'\\';
+file = io.open(abs_path+r"..\..\..\..\tests\data\example_measurement\001_Tracking.txt", "r",encoding='windows-1252') 
+print(read_mppt_file(file.read()))
 
 
 # file = "/home/a2853/Documents/Projects/nomad/hysprintlab/mppt/0a4d713aea22a9edaf1ac8b98fd5e44e.20211129_ re_02-2.mppt.txt"
